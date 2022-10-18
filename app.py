326662b1debb77d8f26ffb7839d739a3a067fcd7
MANISH59991 <79251379+MANISH59991@users.noreply.github.com>
2022-10-18 22:08:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ This app allows you to predict the bioactivity towards inhibting the `Acetylchol
 
 # Sidebar
 with st.sidebar.header('1. Upload your CSV data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['txt'])
+    uploaded_file = st.sidebar.file_uploader("Upload your input dataset", type=['csv'])
     st.sidebar.markdown("""
 [Example input file](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)
 """)
