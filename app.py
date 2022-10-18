0b0aa9b1ec0878cd70016b65e567403812d8d948
MANISH59991 <79251379+MANISH59991@users.noreply.github.com>
2022-10-19 03:23:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,21 +23,17 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
     return href
 
-df = pd.read_csv('descriptor_list.csv')
-df.head()
-st.table(df)
-X = np.array(df['PubchemFP696'])
-Y = np.array(df['PubchemFP824'])
-load_model=LinearRegression()
-lr=load_model.fit(X,Y)
-prediction = load_model.predict(lr)
-st.header('**Prediction output**')
-prediction_output = pd.Series(lr, name='pIC50')
-molecule_name = pd.Series(load_data[1], name='molecule_name')
-df = pd.concat([molecule_name, prediction_output], axis=1)
-st.write(df)
-st.markdown(filedownload(df), unsafe_allow_html=True)
-break()
+def build_model(input_data):
+    # Reads in saved regression model
+    load_model = pickle.load(open('acetylcholinesterase_model.pkl', 'rb'))
+    # Apply model to make predictions
+    prediction = load_model.predict(input_data)
+    st.header('**Prediction output**')
+    prediction_output = pd.Series(prediction, name='pIC50')
+    molecule_name = pd.Series(load_data[1], name='molecule_name')
+    df = pd.concat([molecule_name, prediction_output], axis=1)
+    st.write(df)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
     
 
 
